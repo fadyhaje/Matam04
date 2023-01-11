e13a32a9623d391bc9e272c6d7958f2563f74467
fadyhaje <103383097+fadyhaje@users.noreply.github.com>
2023-01-11 14:43:34 +0200

    Create Card.h

---
+++ Card.h
@@ -0,0 +1,43 @@
+#ifndef Card_H
+#define Card_H
+#include "../Players/Player.h"
+#include <string>
+
+//Abstract class
+class Card {
+public:
+/*
+* Constructor of Card class
+*
+*
+*/
+Card() = default ;
+
+/*
+* Destructor of Card class
+*
+*
+*/
+virtual ~Card() ;
+
+
+virtual void applyEncounter(Player& player) const=0;
+
+
+virtual Card* clone() const = 0; 
+
+/*
+*
+*prints the information of the card
+*
+*/
+virtual void printInfo(std::ostream& os ) const=0;
+
+
+friend std::ostream& operator<<(std::ostream& os,const Card& card); 
+
+
+
+};
+
+#endif //Card_H
