ad56cd6adbecab006ff3b67fb4421cc9c895230e
mariamassdi <117951268+mariamassdi@users.noreply.github.com>
2023-01-11 18:58:58 +0200

    Update Warrior.h

--- Warrior.h
+++ Warrior.h
@@ -3,11 +3,13 @@
 //
 #include "Player.h"
 
-#ifndef UNTITLED2_WARRIOR_H
-#define UNTITLED2_WARRIOR_H
+#ifndef WARRIOR_H
+#define WARRIOR_H
 class Warrior: public Player{
 public:
-    Warrior(const char* name,int maxHP=100,int force=5) : Player(name,maxHP,force){}
+    Warrior(const char* name,int maxHP=100,int force=5) : Player(name,maxHP,force){
+            m_coins=10;
+}
 
     virtual Player* clone() const;
 
