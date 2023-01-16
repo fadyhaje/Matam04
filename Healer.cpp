13db4d4e9a1fb85754f63572ee7297e43eea5a5e
mariamassdi <117951268+mariamassdi@users.noreply.github.com>
2023-01-16 02:19:41 +0200

    Update Healer.cpp

--- Healer.cpp
+++ Healer.cpp
@@ -14,3 +14,7 @@ Player* Healer::clone() const
     return new Healer(*this);
 
 }
+
+void Healer::printInfo(std::ostream& os ) const{
+    printPlayerDetails(os,m_name,"Healer",m_level,m_force,m_HP,m_coins);
+}
