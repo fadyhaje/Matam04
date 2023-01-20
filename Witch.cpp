196e94a6c185d8f47e77c97ae1983e72ace2b775
fadyhaje <103383097+fadyhaje@users.noreply.github.com>
2023-01-21 00:11:01 +0200

    Update Witch.cpp

--- Witch.cpp
+++ Witch.cpp
@@ -1,33 +1,37 @@
 #include "Witch.h"
 
 
-Witch :: Witch() : BattleCard(DEFAULT_FORCE_Witch,DEFAULT_LOOT_Witch,DEFAULT_DAMAGE_Wicth){
+Witch :: Witch() :BattleCard(DEFAULT_FORCE_Witch,DEFAULT_LOOT_Witch,DEFAULT_DAMAGE_Wicth)
+{
     
 }
 
-Card* Wicth::clone() const { 
-    return new Wicth(*this);
-}
 
 void Wictch :: applyEncounter(Player& player) const{
-    if(m_force > player.getAttackStrength() ){ 
-       // loser
+    if(m_force <= player.getAttackStrength())
+    {
+        player.addCoins(m_loot);
+        player.levelUp();
+        printWinBattle(player.getName(),"Wicth");
+    }
+    else
+    {
        player.damage(m_damage);
        player.damageForce();
        printLossBattle(player.getName(),"Wicth");
-
-    }
-    else {
-        //winner
-        player.addCoins(m_loot);
-        player.levelUp();
-        printWinBattle(player.getName(), "Wicth");
+        
     }
 }
 
 void Wicth::printInfo(std::ostream& os ) const{
-    printCardDetails(os, "Wicth");
+    printCardDetails(os,"Wicth");
     printMonsterDetails(os,m_force,m_damage,m_loot);
     printEndOfCardDetails(os);
 
+}
+
+Card* Wicth::clone() const
+{ 
+    Witch temp=new Witch(*this);
+    return temp;
 }
