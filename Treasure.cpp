a7224f3202e9fa1a03ee0b4adb9bc681576f5692
fadyhaje <103383097+fadyhaje@users.noreply.github.com>
2023-01-21 00:17:23 +0200

    Update Treasure.cpp

--- Treasure.cpp
+++ Treasure.cpp
@@ -1,20 +1,24 @@
 #include "Treasure.h"
 
-Treasure :: Treasure() : m_loot(DEFAULT_COINS_TREASURE){
+Treasure :: Treasure():m_loot(DEFAULT_COINS_TREASURE)
+{
 }
 
-
-void Treasure :: applyEncounter (Player& player) const{
+void Treasure :: applyEncounter (Player& player) const
+{
    player.addCoins(m_loot);
    printTreasureMessage();
 }
 
-Card* Treasure::clone() const { 
-   return new Treasure(*this);
+void Treasure::printInfo(std::ostream& os) const
+{
+   printCardDetails(os,"Treasure");
+   printEndOfCardDetails(os);
 }
 
-void Treasure::printInfo(std::ostream& os ) const{
-   printCardDetails(os, "Treasure");
-   printEndOfCardDetails(os);
+Card* Treasure::clone() const 
+{ 
+   Treasure temp=new Treasure(*this);
+   return temp;
 }
 
