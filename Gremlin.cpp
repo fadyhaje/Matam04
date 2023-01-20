90c7c320da9bb2c2970388640426c586000b66b4
fadyhaje <103383097+fadyhaje@users.noreply.github.com>
2023-01-21 00:27:01 +0200

    Update Gremlin.cpp

--- Gremlin.cpp
+++ Gremlin.cpp
@@ -1,31 +1,33 @@
 #include "Gremlin.h"
 
 
-Gremlin :: Gremlin() : BattleCard(DEFAULT_FORCE_Gremlin,DEAFULT_LOOT_Gremlin,DEFAULT_DAMAGE_Gremlin){
-    
+Gremlin ::Gremlin():BattleCard(DEFAULT_FORCE_Gremlin,DEAFULT_LOOT_Gremlin,DEFAULT_DAMAGE_Gremlin)
+{  
 }
 
 void Gremlin :: applyEncounter(Player& player) const {
-    if(m_force > player.getAttackStrength() ){
-       // loser
-       player.damage(m_damage);
-       printLossBattle(player.getName(),"Gremlin");
-    }
-    else {
-        //winner
+    
+    if(m_force<=player.getAttackStrength())
+    {
         player.addCoins(m_loot);
         player.levelUp();
         printWinBattle(player.getName(), "Gremlin");
     }
-
+    else
+    {
+        player.damage(m_damage);
+       printLossBattle(player.getName(),"Gremlin");
+    }
 }
 
-Card* Gremlin::clone() const { 
-    return new Gremlin(*this);  
-}
 
-
-void Gremlin::printInfo(std::ostream& os ) const{
+void Gremlin::printInfo(std::ostream& os ) const
+{
     printCardDetails(os, "Gremlin");
     printMonsterDetails(os,m_force,m_damage,m_loot);
+}
+
+Card* Gremlin::clone() const
+{ 
+    return new Gremlin(*this);  
 }
