801b7c6612c79294234b5d70df5dc8afdcbf031f
fadyhaje <103383097+fadyhaje@users.noreply.github.com>
2023-01-21 00:30:59 +0200

    Update Barfight.cpp

--- Barfight.cpp
+++ Barfight.cpp
@@ -3,27 +3,33 @@
 #include "../Players/Warrior.h"
 #include "../utilities.h"
 
-Barfight :: Barfight(): m_damage( DEFAULT_DAMAGE_BARFIGHT) {} 
+Barfight :: Barfight(): m_damage( DEFAULT_DAMAGE_BARFIGHT)
+{
+} 
 
 
 
-Card* Barfight::clone() const { 
-    return new Barfight(*this);  
-}
 
-
-void Barfight :: applyEncounter(Player& player) const{
+void Barfight ::applyEncounter(Player& player) const{
     const Warrior* warrior = dynamic_cast<const Warrior*>(&player);
-    if (warrior == nullptr) {
+    if (warrior!=nullptr)
+    {
+        printBarfightMessage(true);
+    }
+    else
+    {
         player.damage(m_damage);
         printBarfightMessage(false);
     }
-    else{
-        printBarfightMessage(true);
-    }
 }
 
-void Barfight::printInfo(std::ostream& os ) const{
+void Barfight::printInfo(std::ostream& os) const
+{
     printCardDetails(os, "Barfight");
     printEndOfCardDetails(os);
+}
+
+Card* Barfight::clone() const
+{ 
+    return new Barfight(*this);  
 }
