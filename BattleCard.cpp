9c7be33fcb4dc2983e0d023725f736945d076cb3
fadyhaje <103383097+fadyhaje@users.noreply.github.com>
2023-01-21 00:29:36 +0200

    Update BattleCard.cpp

--- BattleCard.cpp
+++ BattleCard.cpp
@@ -1,6 +1,6 @@
 #include "BattleCard.h"
 
-BattleCard :: BattleCard(int force, int loot, int damage): m_force(force),m_loot(loot),m_damage(damage) {
-   
+BattleCard ::BattleCard(int force, int loot, int damage):m_force(force),m_loot(loot),m_damage(damage)
+{
 }
 
