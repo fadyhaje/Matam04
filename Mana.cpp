90bb2dc058e7ef632bb91e00e63c650177cdb510
fadyhaje <103383097+fadyhaje@users.noreply.github.com>
2023-01-21 00:20:37 +0200

    Update Mana.cpp

--- Mana.cpp
+++ Mana.cpp
@@ -1,15 +1,15 @@
 #include "Mana.h"
 
 
-Mana :: Mana(){
-    m_heal = DEFAULT_HEAL_MANA;  
+Mana :: Mana()
+{
+    m_heal=DEFAULT_HEAL_MANA;  
 }
 
-Card* Mana::clone() const { 
-    return new Mana(*this);  
-}
 
-void Mana :: applyEncounter(Player& player) const{
+
+void Mana :: applyEncounter(Player& player) const
+{
     const  Healer* healer = dynamic_cast<const  Healer*>(&player);
     if (healer !=nullptr) {
         player.heal(m_heal);
@@ -23,5 +23,10 @@ void Mana :: applyEncounter(Player& player) const{
 void Mana::printInfo(std::ostream& os ) const{
    printCardDetails(os, "Mana");
    printEndOfCardDetails(os);
+}
+
+Card* Mana::clone() const 
+{ 
+    return new Mana(*this);  
 }
 
