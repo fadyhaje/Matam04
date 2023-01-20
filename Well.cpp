c9550b99f187c5212caa48a3925ce737b8e7b8ac
fadyhaje <103383097+fadyhaje@users.noreply.github.com>
2023-01-21 00:13:52 +0200

    Update Well.cpp

--- Well.cpp
+++ Well.cpp
@@ -1,27 +1,32 @@
+#include "Well.h"
 
-
-Well :: Well(){
+Well :: Well()
+{
     m_damage = DEFAULT_DAMAGE_Well;  
 }
 
 
-void Well :: applyEncounter(Player& player)const {
+void Well :: applyEncounter(Player& player)const 
+{
     const Ninja* ninja = dynamic_cast<const Ninja*>(&player);
-    if (ninja == nullptr) {
+    if(ninja !=nullptr){
+        printWellMessage(true);
+    }
+    else{
         player.damage(m_damage);
         printWellMessage(false);
     }
-    else{
-        printWellMessage(true);
-    }
 }
 
-Card* Well::clone() const { 
-    return new Well(*this);  
-}
-
-void Well::printInfo(std::ostream& os ) const{
-    printCardDetails(os, "Well");
+void Well::printInfo(std::ostream& os ) const
+{
+    printCardDetails(os,"Well");
     printEndOfCardDetails(os);
+}
+
+Card* Well::clone() const
+{ 
+    Well temp=new Well(*this);
+    return Well;  
 }
 
