bc7051c858a8eec3a1ddd6e9edbe20e4f5dd9cf6
fadyhaje <103383097+fadyhaje@users.noreply.github.com>
2023-01-21 12:02:54 +0200

    Update Merchant.cpp

--- Merchant.cpp
+++ Merchant.cpp
@@ -2,67 +2,73 @@
 
 
 Merchant :: Merchant (){
-  m_hp = DEFAULT_HP_MERCHANT;
-  m_force = DEFAULT_FORCE_MERCHANT;
+  m_hp = DEFAULT_HP_FOR_MERCHANT;
+  m_force = DEFAULT_FORCE_FOR_MERCHANT;
+}
+void Merchant ::printInfo(std::ostream& os) const
+{
+  printCardDetails(os,"Merchant");
+  printEndOfCardDetails(os);
 }
 
-void Merchant :: applyEncounter(Player& player) const{
+void Merchant ::applyEncounter(Player& player) const{
     printMerchantInitialMessageForInteractiveEncounter(std::cout,player.getName(),player.getCoins());
-    int input;
-    bool valid = false;
-    while(!valid)
+    bool available = false;
+    int temp_input;
+    while(available==false)
     {
-      std::string number;
       std::getline( std::cin,number );
-      try{
-        input = std::stoi(number);
+      std::string number;
+      try
+      {
+        temp_input = std::stoi(number);
       }
-      catch(...){
+      catch(...)
+      {
         printInvalidInput();
         continue;
       }
-        if(input !=0 && input!=1 && input!= 2 ){
-          printInvalidInput();
-        }
-        else{
-          valid = true;
-        }
-    }
-    if(input == 1){
-      if(player.getCoins()>=HP_PRICE_MERCHANT){
-        // paying for hp 
-        player.pay(HP_PRICE_MERCHANT);
-        player.heal(m_hp);
+      if(temp_input==0 || temp_input==1 || temp_input==2)
+      {
+        available = true;
       }
-      else{
-        // the player does not have enough money
-        printMerchantInsufficientCoins(std::cout);
-        printMerchantSummary(std::cout,player.getName(),1,0);
-        return;
+      else
+      {
+        printInvalidInput();
       }
     }
-    else if (input == 2){
-        if(player.getCoins()>=FORCE_PRICE_MERCHANT){
-          // paying for force 
-          player.pay(FORCE_PRICE_MERCHANT);
-          player.buff(m_force); 
-        }
-        else{
-          // the player does not have enough money
+  switch(temp_input){
+    case 2:
+        if(player.getCoins()<FORCE_PRICE_MERCHANT)
+        {
           printMerchantInsufficientCoins(std::cout);
           printMerchantSummary(std::cout,player.getName(),2,0);
           return;
         }
-    }
-    printMerchantSummary(std::cout,player.getName(),input,input*5);
+        else
+        {
+          player.pay(FORCE_PRICE_MERCHANT);
+          player.buff(m_force); 
+        }
+        break;
+    case 1:
+       if(player.getCoins()<HP_PRICE_MERCHANT)
+       {
+         printMerchantInsufficientCoins(std::cout);
+         printMerchantSummary(std::cout,player.getName(),1,0);
+         return;
+      }
+      else
+      {
+        player.pay(HP_PRICE_MERCHANT);
+        player.heal(m_hp);
+      }
+      break;
+  }
+  printMerchantSummary(std::cout,player.getName(),input,input*5);
 }
 
-void Merchant::printInfo(std::ostream& os ) const{
-  printCardDetails(os, "Merchant");
-  printEndOfCardDetails(os);
-}
-
-Card* Merchant::clone() const
+Card* Merchant ::clone() const
 { 
   return new Merchant(*this); 
 }
