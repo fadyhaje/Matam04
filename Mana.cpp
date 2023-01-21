e8e7f69262ac35f78a616e151499408de57cbabd
fadyhaje <103383097+fadyhaje@users.noreply.github.com>
2023-01-21 10:22:04 +0200

    Update Mana.cpp

--- Mana.cpp
+++ Mana.cpp
@@ -6,22 +6,19 @@ Mana :: Mana()
     m_heal=DEFAULT_HEAL_MANA;  
 }
 
-
-
 void Mana :: applyEncounter(Player& player) const
 {
     const  Healer* healer = dynamic_cast<const  Healer*>(&player);
     if (healer !=nullptr) {
         player.heal(m_heal);
         printManaMessage(true);
+        return;
     }
-    else{
-       printManaMessage(false);
-    }
+    printManaMessage(false);
 }
 
 void Mana::printInfo(std::ostream& os ) const{
-   printCardDetails(os, "Mana");
+   printCardDetails(os,"Mana");
    printEndOfCardDetails(os);
 }
 
