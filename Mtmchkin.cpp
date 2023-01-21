9a16bab8fbbf5c5364e5ce2c5bd84cff58dfc735
fadyhaje <103383097+fadyhaje@users.noreply.github.com>
2023-01-21 17:36:30 +0200

    Update Mtmchkin.cpp

--- Mtmchkin.cpp
+++ Mtmchkin.cpp
@@ -86,8 +86,6 @@ void createCardDeck(const std::string &fileName,queue<unique_ptr<Card>>& cardDec
 }
 
 bool isvalidPlayerNum(int numOfPlayer){
-
-
     if(numOfPlayer<2||numOfPlayer>6)
     {
         printInvalidTeamSize();
@@ -98,6 +96,7 @@ bool isvalidPlayerNum(int numOfPlayer){
         return true;
     }
  }
+
 bool addPlayer(queue<unique_ptr<Player>>& Players,string& playerClass,string& playerName){
      if(playerClass=="Ninja")
      {
@@ -121,6 +120,9 @@ bool addPlayer(queue<unique_ptr<Player>>& Players,string& playerClass,string& pl
          return false;
      }
  }
+
+
+
 bool islegalchars(string& name)
 {
     for (int i = 0; i < name.size(); ++i) {
@@ -129,6 +131,7 @@ bool islegalchars(string& name)
     }
     return true;
 }
+//handle_players_name
 bool isValidPlayerName(string& playerName){
      if(playerName.size()>15|| !islegalchars(playerName))
      {
@@ -140,286 +143,361 @@ bool isValidPlayerName(string& playerName){
 
 
 #include "Mtmchkin.h"
-#include "utilities.h"
 #include <map>
-
-#include "Players/Wizard.h"
-#include "Players/Fighter.h"
-#include "Players/Rogue.h"
+#include "utilities.h"
+#include "Players/Ninja.h"
+#include "Players/Healer.h"
+#include "Players/Warrior.h"
+#include "Cards/Gremlin.h"
+#include "Cards/Witch.h"
+#include "Cards/Dragon.h"
 #include "Cards/Merchant.h"
 #include "Cards/Treasure.h"
-#include "Cards/Pitfall.h"
+#include "Cards/Well.h"
 #include "Cards/Barfight.h"
-#include "Cards/Dragon.h"
-#include "Cards/Fairy.h"
-#include "Cards/Goblin.h"
-#include "Cards/Vampire.h"
+#include "Cards/Mana.h"
 #include "Exception.h" 
 
-#define MAX_LETTERS_NUMBER 15
+#define UPPER 1
+#define LOWER -1
+#define MAX_NUM_OF_LETTERS 15
 #define MAX_LEVEL 10
-#define MOVE_RIGHT 1
-#define MOVE_LEFT -1
-#define MINIMUM_NUMBER_OF_CARDS 5
+#define MIN_CARDS_NUM 5
 
-using std::ifstream;
-using std::cin;
 using std::find;
+using std::cin;
+using std::ifstream;
 
-//return a valid number that represents the players's number
 static int handle_players_number(){
-   int players_number;
-   printEnterTeamSizeMessage();
-   bool valid=false;
-   while(!valid){
-      std::string number;
-      std::getline( std::cin,number );
-      try{
-         players_number = std::stoi(number);
-      }
-      catch(...){
-         printInvalidTeamSize();
-         printEnterTeamSizeMessage();
-         continue;
-      }
-   
-      if((players_number >= MIN_NUM_OF_PLAYERS) && (players_number <= MAX_NUM_OF_PLAYERS)){
-         return players_number;
-      }
-      else{
-         printInvalidTeamSize();
-         printEnterTeamSizeMessage();
-      }
-   }
-   return 0;// won't get here
+	int players_number;
+	printEnterTeamSizeMessage();
+	bool valid=false;
+	while(!valid){
+		std::string number;
+		std::getline( std::cin,number );
+		try{
+			players_number = std::stoi(number);
+		}
+		catch(...){
+			printInvalidTeamSize();
+			printEnterTeamSizeMessage();
+			continue;
+		}
+		if((players_number >= MIN_NUM_OF_PLAYERS) && (players_number <= MAX_NUM_OF_PLAYERS)){
+			return players_number;
+		}
+		else{
+			printInvalidTeamSize();
+			printEnterTeamSizeMessage();
+		}
+	}
+	return 0;
 }
 
-// checks if the name is OK   
-static bool handle_players_name(std::string *playerName){
-   if(playerName->length()>MAX_LETTERS_NUMBER) {
-      printInvalidName();
-      return false;
-   }
-   for(std::size_t i=0;i<playerName->length();i++){
-      char letter=playerName->at(i);
-      if ((letter >= 'A' && letter <= 'Z' ) || ( letter >= 'a' && letter <= 'z' ))
-      {
-      continue;  // cheking valid letters
-      }  
-      else{
-         printInvalidName();  
-         return false;
-      }
-   }
-   return true;
-}
 
-//adds the player to the game if the player's class is OK 
+/*
+static bool handle_players_name(std::string *playerName){	
+	if(playerName->length()>MAX_NUM_OF_LETTERS) {
+		printInvalidName();
+		return false;
+	}
+	for(std::size_t i=0;i<playerName->length();i++){
+		char letter=playerName->at(i);
+		if ((letter >= 'A' && letter <= 'Z' ) || ( letter >= 'a' && letter <= 'z' ))
+		{
+			continue; 
+		}  
+		else{
+			printInvalidName();  
+			return false;
+		}
+	}
+	return true;
+}*/
+
+
 static bool handle_players_class(std::string playerClass,std::string name, std::unique_ptr<Player> players[],int place){
-   std::map<std::string, int> play;
-   play["Rogue"] = 0;
-   play["Wizard"] = 1;
-   play["Fighter"] = 2;
-   if(!play.count(playerClass)){
-      printInvalidClass();
-      return false;
-   }
-   switch (play[playerClass]){
-      case 0:
-         players[place]=std::unique_ptr<Rogue>(new Rogue(name));
-         break;
-      case 1:
-         players[place]=std::unique_ptr<Wizard>(new Wizard(name));
-         break;
-      case 2:
-         players[place]=std::unique_ptr<Fighter>(new Fighter(name));
-         break;
-      default:
-         printInvalidClass();
-         return false;
-      }
-      return true;
+	std::map<std::string, int> play;
+	play["Ninja"] = 0;
+	play["Healer"] = 1;
+	play["Warrior"] = 2;
+	if(!play.count(playerClass)){
+		printInvalidClass();
+		return false;
+	}
+	switch (play[playerClass]){
+		case 0:
+			players[place]=std::unique_ptr<Rogue>(new Ninja(name));
+			break;
+		case 1:
+			players[place]=std::unique_ptr<Wizard>(new Healer(name));
+			break;
+		case 2:
+			players[place]=std::unique_ptr<Fighter>(new Warrior(name));
+			break;
+		default:
+			printInvalidClass();
+			return false;
+	}
+	return true;
 }
+
+static bool islegalchars(string& name)
+{
+    for (int i = 0; i < name.size(); ++i) {
+        if(!(name[i]<='z'||name[i]>='a')||!(name[i]<='Z'&&name[i]>='A')&&name[i]==' ')
+            return false;
+    }
+    return true;
+}
+//handle_players_name
+static bool isValidPlayerName(string& playerName){
+     if(playerName.size()>MAX_NUM_OF_LETTERS|| !islegalchars(playerName))
+     {
+         printInvalidName();
+         return false;
+     }
+     return true;
+ }
 
 
 static void handle_players_details(std::unique_ptr<Player> players[],int players_num ){
-   std::string player_Details;
-   std::string current_name;
-   std::string current_job;
-   bool valid_class= false,valid_name=false;
-   for(int i=0;i<players_num;i++){
-      printInsertPlayerMessage();
-      while(!valid_class){
-         while(!valid_name){
-           std::getline(std::cin, player_Details);
-           std::size_t ch=player_Details.find(" ",0);
-           current_name= player_Details.substr(0,ch);
-           current_job=player_Details.substr(ch+1);
-           valid_name=handle_players_name(&current_name);
-         }
-         valid_class = handle_players_class(current_job,current_name,players,i);
-         if(!valid_class){
-           valid_name=false;
-         }
-      }
-      valid_class=false;
-      valid_name=false;
-   }
+	std::string player_Details;
+	std::string current_name;
+	std::string current_job;
+	bool valid_class= false,valid_name=false;
+	for(int i=0;i<players_num;i++){
+		printInsertPlayerMessage();
+		while(!valid_class){
+			while(!valid_name){
+				std::getline(std::cin, player_Details);
+				std::size_t ch=player_Details.find(" ",0);
+				current_name= player_Details.substr(0,ch);
+				current_job=player_Details.substr(ch+1);
+				valid_name=isValidPlayerName(current_name);
+			}
+			valid_class = handle_players_class(current_job,current_name,players,i);
+			if(!valid_class){
+				valid_name=false;
+			}
+		}
+		valid_class=false;
+		valid_name=false;
+	}
 }
 
 
 Mtmchkin:: Mtmchkin(const std::string &fileName){
-   printStartGameMessage();
-   m_roundsNumber=0;
-   ifstream source(fileName);
-   if (!source) {
-	   throw DeckFileNotFound();
-   }
-   std::map<std::string, int> cards;
-   cards["Fairy"] = 0;
-   cards["Dragon"] = 1;
-   cards["Vampire"] = 2;
-   cards["Goblin"] = 3;
-   cards["Pitfall"] = 4;
-   cards["Barfight"] = 5;
-   cards["Treasure"] = 6;
-   cards["Merchant"] = 7;
-   std::string line;
-   int countNumOfCards=0;
-   while(getline(source,line)){
-      countNumOfCards++;
-      if(!cards.count(line)){
-         throw DeckFileFormatError(countNumOfCards);
-      }
-      switch(cards[line]){
-         case 0:
-            m_cards.push(std::unique_ptr<Fairy>(new Fairy()));
-            break; 
-         case 1:   
-            m_cards.push(std::unique_ptr<Dragon>(new Dragon()));
-            break; 
-         case 2:
-            m_cards.push(std::unique_ptr<Vampire>(new Vampire()));
-            break; 
-         case 3:
-            m_cards.push(std::unique_ptr<Goblin>(new Goblin()));
-            break; 
-         case 4:
-            m_cards.push(std::unique_ptr<Pitfall>(new Pitfall()));
-            break; 
-         case 5:
-            m_cards.push(std::unique_ptr<Barfight>(new Barfight()));
-            break; 
-         case 6:
-            m_cards.push(std::unique_ptr<Treasure>(new Treasure()));
-            break; 
-         case 7:
-            m_cards.push(std::unique_ptr<Merchant>(new Merchant()));
-            break; 
-         default:
-            throw DeckFileFormatError(countNumOfCards);
-            //catch in main and break;*/
-         }
-      }
-      if (countNumOfCards<MINIMUM_NUMBER_OF_CARDS){
-         throw DeckFileInvalidSize();
-      }
-      m_playersNumber=handle_players_number();
-      handle_players_details(m_players,m_playersNumber);
-      for(int i=0;i<m_playersNumber;i++){
-         m_ranking[i]=i;
-      }
-   }
+	printStartGameMessage();
+	m_roundsNumber=0;
+	ifstream source(fileName);
+	if (!source) {
+		throw DeckFileNotFound();
+	}
+	std::map<std::string, int> cards;
+	cards["Mana"] = 0;
+	cards["Dragon"] = 1;
+	cards["Witch"] = 2;
+	cards["Gremlin"] = 3;
+	cards["Well"] = 4;
+	cards["Barfight"] = 5;
+	cards["Treasure"] = 6;
+	cards["Merchant"] = 7;
+	std::string line;
+	int countNumOfCards=0;
+	while(getline(source,line)){
+		countNumOfCards++;
+		if(!cards.count(line)){
+			throw DeckFileFormatError(countNumOfCards);
+		}
+		switch(cards[line]){
+			case 0:
+				m_cards.push(std::unique_ptr<Fairy>(new Mana()));
+				break; 
+			case 1:   
+				m_cards.push(std::unique_ptr<Dragon>(new Dragon()));
+				break; 
+			case 2:
+				m_cards.push(std::unique_ptr<Vampire>(new Witch()));
+				break; 
+			case 3:
+				m_cards.push(std::unique_ptr<Goblin>(new Gremlin()));
+				break; 
+			case 4:
+				m_cards.push(std::unique_ptr<Pitfall>(new Well()));
+				break; 
+			case 5:
+				m_cards.push(std::unique_ptr<Barfight>(new Barfight()));
+				break; 
+			case 6:
+				m_cards.push(std::unique_ptr<Treasure>(new Treasure()));
+				break; 
+			case 7:
+				m_cards.push(std::unique_ptr<Merchant>(new Merchant()));
+				break; 
+			default:
+				throw DeckFileFormatError(countNumOfCards);
+		}
+	}
+	if (countNumOfCards< MIN_CARDS_NUM){
+		throw DeckFileInvalidSize();
+	}
+	m_playersNumber=handle_players_number();
+	handle_players_details(m_players,m_playersNumber);
+	for(int i=0;i<m_playersNumber;i++){
+		m_ranking[i]=i;
+	}
+}
    
-static bool is_playing(const Player& player){
-   return ((player.getLevel() < MAX_LEVEL) && (!player.isKnockedOut()));
-}
-
-static void swap(int ranking[],int src,int dest){
-  int temp=ranking[src];
-  ranking[src]=ranking[dest];
-  ranking[dest]=temp; 
-}
-
-
-static int getindex(int ranking[],int index,int playersNum){
-   for(int i=0;i<playersNum;i++){
-      if(ranking[i]==index){
-         return i;
-      }
-   }
-   return 0;//won't get here
-}
-
-static void rearrange_the_players(std::unique_ptr<Player> players[],int ranking[],int index,int playersNum)
+/*static bool is_playing(const Player& player)
 {
-   if(is_playing(*(players[index]))){
-      return; // the player is still playing
-   }
-   int direction;
-   if((*(players[index])).getLevel()==MAX_LEVEL){
-      direction =MOVE_LEFT;
-   }
-   else{
-      assert((*(players[index])).isKnockedOut());
-      direction =MOVE_RIGHT;
-   }
-   int i=getindex(ranking,index,playersNum);
-   while((i+direction)>=0 && (i+direction)<playersNum){
-      if(is_playing(*(players[ranking[i+direction]]))){
-         swap(ranking,i,i+direction);
-         i=getindex(ranking,index,playersNum);
-      }
-      else {
-         break;
-      }
-   }
-   return;
+	return ((player.isKnockedOut()==false) &&(player.getLevel()<MAX_LEVEL));
+}
+
+static void swap(int ranking[],int src,int dest)
+{
+	int curren_rank=ranking[src];
+	ranking[src]=ranking[dest];
+	ranking[dest]=curren_rank; 
+}*/
+
+
+static int get_sorted_index(int sorted[],int number_of_players,int index)
+{
+	for(int i=0;i<numer_of_players;i++)
+	{
+		if(sorted[i]!=index)
+		{
+			continue;
+		}
+		else
+		{
+			return i;
+		}
+	}
+	return 0;
+}
+
+static void players_new_sort(std::unique_ptr<Player> players[],int sorted[],int number_of_players,int index)
+{
+	int way;
+	if((players[index]->isKnockedOut()==false) &&(players[index]->getLevel()<MAX_LEVEL))
+	{
+		return; 
+	}
+	int sorted_index=get_sorted_index(sorted,number_of_players,index)
+	if(players[index]->getLevel()!=MAX_LEVEL)
+	{
+		way=UPPER;
+	}
+	else
+	{
+		way=LOWER;
+	}
+	int temp_index=sorted_index+way,current_rank;
+	bool flag=temp_index<number_of_players && temp_index>=0;
+	while(flag){
+		if(!((players[sorted[i+way]]->isKnockedOut()==false) && (players[sorted[i+way]]->getLevel()<MAX_LEVEL)))
+		{
+			break;
+		}
+		curren_rank=dorted[sorted_index];
+	        sorted[sorted_index]=sorted[sorted_index+way];
+	        sorted[sorted+index]=curren_rank; 
+		sorted_index=get_sorted_index(sorted,number_of_players,index);
+		temp_index=sorted_index+way;
+		flag=temp_index<number_of_players && temp_index>=0;
+	}
+	return;
 }
 
 
-void Mtmchkin:: playRound(){
-   m_roundsNumber++;
-   printRoundStartMessage( m_roundsNumber);
-   for(int i = 0 ; i < m_playersNumber ; i++)
-     {
-      if(is_playing(*(m_players[i]))){
-         printTurnStartMessage((*(m_players[i])).getName()); 
-         (*(m_cards.front())).applyEncounter(*(m_players[i]));
-         m_cards.push(std::move(m_cards.front())); // add the first card to the end of the deck
-         m_cards.pop(); // remove the first card from the start of the deck
-         rearrange_the_players(m_players,m_ranking,i,m_playersNumber);
-        }
-    }
-      if(isGameOver()){
-         printGameEndMessage();
-      }
+void Mtmchkin ::playRound()
+{ 
+	printRoundStartMessage(++m_roundsNumber);
+	int i=0;
+	bool game_final;
+	while(i<m_playersNumber)
+	{
+		if(!((m_players[i]->isKnockedOut()==false) &&(m_players[i]->getLevel()<MAX_LEVEL)))
+		{
+			i++;
+			continue;
+		}
+		else
+		{
+			std::unique_ptr<Card> firstCard= m_cards.front();
+			printTurnStartMessage(std::string(*m_players[i]).getName())); 
+			firstCard->applyEncounter(*m_players[i]);
+			m_cards.push(first_card); 
+			m_cards.pop();
+			players_new_sort(m_players,m_ranking,m_playersNumber,i);
+			i++;
+		}
+	}
+	game_final=isGameOver();
+	if(game_final)
+	{
+		printGameEndMessage();
+	}
 }
 
 
-bool Mtmchkin :: isGameOver() const{
-    for (int i = 0; i < m_playersNumber ; i++)
-    {
-        if(is_playing(*(m_players[i])))
-        {
-            // there is a player who is still playing 
-            // --> the game is not over 
+bool Mtmchkin ::isGameOver() const
+{
+	int i=0;
+	bool flag=false;
+	while(i<m_playersNumber)
+	{
+		if((m_players[i]->isKnockedOut()==false) &&(m_players[i]->getLevel()<MAX_LEVEL)){
+			flag=true;
+			break;
+		}
+		else{
+			i++;
+			continue;
+		}
+	}
+	if(flag==true){
+		return false;
+	}
+	printGameEndMessage();
+	return true;
+}
+
+void Mtmchkin::printLeaderBoard() const
+{
+	int i=1;
+	std::unique_ptr<Player> current_player;
+	printLeaderBoardStartMessage();
+	while(i<m_playersNumber)
+	{
+		current_player=m_players[m_sorted[i-1]];
+		printPlayerLeaderBoard(i, *current_player);
+		i++;
+	}
+}
+
+/////////////////////////////////////////////
+int Mtmchkin ::getNumerOfRounds() const
+{
+	return m_roundsNumber;
+}
+
+static bool islegalchars(string& name)
+{
+    for (int i = 0; i < name.size(); ++i) {
+        if(!(name[i]<='z'||name[i]>='a')||!(name[i]<='Z'&&name[i]>='A')&&name[i]==' ')
             return false;
-        }
     }
-    // all the players are not playing 
-    // --> the game is over
     return true;
+}
+//handle_players_name
+static bool isValidPlayerName(string& playerName){
+     if(playerName.size()>MAX_NUM_OF_LETTERS|| !islegalchars(playerName))
+     {
+         printInvalidName();
+         return false;
+     }
+     return true;
  }
-
-int Mtmchkin::getNumberOfRounds() const{
-   return m_roundsNumber;
-}
-
-void Mtmchkin::printLeaderBoard() const{
-   printLeaderBoardStartMessage();
-   for(int i=0;i<m_playersNumber;i++){
-      printPlayerLeaderBoard(i+1, *(m_players[m_ranking[i]]));
-   }
-}
-
