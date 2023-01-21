845f3038d3f69fb0fb2cb4788b9b395d1be7c968
fadyhaje <103383097+fadyhaje@users.noreply.github.com>
2023-01-21 11:25:32 +0200

    Update Ninja.cpp

--- Ninja.cpp
+++ Ninja.cpp
@@ -8,10 +8,6 @@ void Ninja::addCoins(int coins)
     Player::addCoins(2*coins);
 }
 
-void Ninja::printInfo(std::ostream& os) const
-{
-    printPlayerDetails(os,m_name,"Ninja",m_level,m_force,m_HP,m_coins);
-}
 
 Player* Ninja::clone() const
 {
