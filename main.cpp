eb76558da1893543acb81911b809ecd7eb9fb32b
fadyhaje <103383097+fadyhaje@users.noreply.github.com>
2023-01-21 10:39:57 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <fstream>
+#include <iostream>
+#include "../Mtmchkin.h"
+#include "../Exception.h"
+using std::endl;
+using std::cout;
+
+int main()
+{
+    try{
+        Mtmchkin game("deck.txt");
+        while(game.isGameOver()==false)
+        {
+           game.playRound();
+           game.printLeaderBoard();
+        }
+    }
+    catch(const DeckFileFormatError& e){
+        cout << e.what() <<endl;
+        return 1;
+    }
+    catch(const DeckFileNotFound& e){
+        cout << e.what() <<endl;
+        return 1;
+    }
+    catch(const DeckFileInvalidSize& e){
+        cout << e.what() <<endl;
+        return 1;
+    }
+    catch (const std::bad_alloc& e){
+        cout << e.what() << endl;
+        return 1;
+    }
+    return 0;
+}
+
