0da981ec22cbdd7311e22313f4d8cb2048ca867b
fadyhaje <103383097+fadyhaje@users.noreply.github.com>
2023-01-21 11:27:37 +0200

    Update Ninja.h

--- Ninja.h
+++ Ninja.h
@@ -6,23 +6,21 @@
 #define NINJA_H
 
 #include "Player.h"
+#include <stdbool.h>
+#include <string>
+#include "../utilities.h"
+#include "Player.h"
 
+using std:string;
 class Ninja: public Player{
 public:
-    Ninja(const string name,int maxHP=100,int force=5) : Player(name,maxHP,force){
-            m_coins=10;
-}
+    Ninja(string name) : Player(name){}
 
     virtual Player* clone() const;
 
     void addCoins(int money) override;
 
     ~Ninja() =default;
-    
-        void printInfo(std::ostream& os ) const override;
-
-
-
 
 };
 #endif //UNTITLED2_NINJA_H
