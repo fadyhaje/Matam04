98fd2aa5209d50cbd9a03b5c6d420c5fc33e051d
fadyhaje <103383097+fadyhaje@users.noreply.github.com>
2023-01-21 11:28:21 +0200

    Update Healer.cpp

--- Healer.cpp
+++ Healer.cpp
@@ -8,12 +8,6 @@ void Healer::heal(int addtional_HP)
     Player::heal(2*addtional_HP);
 }
 
-
-void Healer ::printInfo(std::ostream& os) const
-{
-    printPlayerDetails(os,m_name,"Healer",m_level,m_force,m_HP,m_coins);
-}
-
 Player* Healer::clone() const
 {
     return new Healer(*this);
