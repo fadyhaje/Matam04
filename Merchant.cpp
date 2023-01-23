70662dc2fb7a808561646455fb0136ab32433076
mariamassdi <117951268+mariamassdi@users.noreply.github.com>
2023-01-23 19:23:03 +0200

    Update Merchant.cpp

--- Merchant.cpp
+++ Merchant.cpp
@@ -1,57 +1,86 @@
-#ifndef Merchant_H
-#define Merchant_H
-
-#include "Card.h"
-#include <iostream>
-#include <stdbool.h>
-#include "../utilities.h"
-
-#define HP_PRICE 5
-#define FORCE_PRICE 10
-#define DEFAULT_HP 1
-#define DEFAULT_FORCE 1
-
-class Merchant : public Card{
-public:
-    /*
-    * C'tor of Mtmchkin class
-    *
-    * @return
-    *      A new Merchant object.
-    */
-    Merchant();
+#include "Merchant.h"
 
 
-    void applyEncounter(Player& player) const override;
-    /*
-    *
-    *prints the card's information- according to the instruction in the exercise document.
-    *
-    * @return
-    *      void
-    */
-    void printInfo(std::ostream& os ) const override;
 
-    /*
-    * create a new copy of Merchant by using the copy constructor
-    *
-    * return:
-    *   pointer to a copy of Merchant
-    */
-    Card* clone() const override;
+Merchant ::Merchant ()
+{
+    m_force = DEFAULT_FORCE;
+    m_hp = DEFAULT_HP;
+}
 
-    /*
-   * Destructor of Merchant class
-   *
-   *
-   */
-    ~Merchant()=default;
+void Merchant::checker_1(Player& player) const{
+    if(player.getCoins()<HP_PRICE)
+    {
+        printMerchantInsufficientCoins(std::cout);
+        printMerchantSummary(std::cout,player.getName(),1,0);
+        return;
+    }
+    else
+    {
+        player.pay(HP_PRICE);
+        player.heal(m_hp);
+    }
+}
 
-private:
-    void checker_1(Player& player) const;
-    void checker_2(Player& player) const;
-    int m_hp;
-    int m_force;
-};
+void Merchant ::printInfo(std::ostream& os) const
+{
+    printCardDetails(os,"Merchant");
+    printEndOfCardDetails(os);
+}
 
-#endif //Merchant_H
+ void Merchant::checker_2(Player& player) const{
+    if(player.getCoins()<FORCE_PRICE)
+    {
+        printMerchantInsufficientCoins(std::cout);
+        printMerchantSummary(std::cout,player.getName(),2,0);
+        return;
+    }
+    else
+    {
+        player.pay(FORCE_PRICE);
+        player.buff(m_force);
+    }
+}
+
+Card* Merchant ::clone() const
+{
+    return new Merchant(*this);
+}
+
+void Merchant ::applyEncounter(Player& player) const{
+    printMerchantInitialMessageForInteractiveEncounter(std::cout,player.getName(),player.getCoins());
+    bool available = false;
+    int temp_input;
+    while(available==false)
+    {
+        std::string number;
+        std::getline( std::cin,number);
+        try
+        {
+            temp_input = std::stoi(number);
+        }
+        catch(...)
+        {
+            printInvalidInput();
+            continue;
+        }
+        if(temp_input>=0 && temp_input<=2)
+        {
+            available = true;
+        }
+        else
+        {
+            printInvalidInput();
+        }
+    }
+    switch(temp_input){
+        case 2:
+           checker_2(player);
+            break;
+        case 1:
+            checker_1(player);
+            break;
+    }
+    printMerchantSummary(std::cout,player.getName(),temp_input,temp_input*5);
+}
+
