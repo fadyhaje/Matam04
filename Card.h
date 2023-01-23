9cc3dd345d6bb95698b5a3cce0328fc9f9832c6b
mariamassdi <117951268+mariamassdi@users.noreply.github.com>
2023-01-23 22:38:47 +0200

    Update Card.h

--- Card.h
+++ Card.h
@@ -7,33 +7,34 @@
 class Card {
 public:
 /*
-* Constructor of Card class
-*
-*
+* Default constructor of Card class
+ * @return
+ * *      A new Card object
 */
-Card() = default ;
+    Card() = default ;
 
 /*
 * Destructor of Card class
 *
 *
 */
-virtual ~Card(){}
-  
-virtual void applyEncounter(Player& player) const=0;
+    virtual ~Card() ;
 
 
-virtual Card* clone() const = 0; 
+    virtual void applyEncounter(Player& player) const=0;
+
+
+    virtual Card* clone() const = 0;
 
 /*
 *
 *prints the information of the card
 *
 */
-virtual void printInfo(std::ostream& os ) const=0;
+    virtual void printInfo(std::ostream& os ) const=0;
 
 
-friend std::ostream& operator<<(std::ostream& os,const Card& card); 
+    friend std::ostream& operator<<(std::ostream& os,const Card& card);
 
 
 
