03f0874c55c363ed6a31408990bc09b1964cbe75
mariamassdi <117951268+mariamassdi@users.noreply.github.com>
2023-01-23 22:39:40 +0200

    Update Dragon.h

--- Dragon.h
+++ Dragon.h
@@ -1,31 +1,28 @@
 #ifndef Dragon_H
 #define Dragon_H
-#include "BattleCard.h" 
+#include "BattleCard.h"
 #include <iostream>
 #include "../utilities.h"
 #include "../Players/Player.h"
 
-#define DEFAULT_FORCE_DRAGON 25
-#define DEAFULT_LOOT_DRAGON 1000
-#define DEFAULT_DAMAGE_DRAGON 100
+#define DEFAULT_FORCE_FOR_DRAGON 25
+#define DEAFULT_LOOT_FOR_DRAGON 1000
+#define DEFAULT_DAMAGE_FOR_DRAGON 100
 
-class Dragon : public BattleCard{ 
-    public:
+class Dragon : public BattleCard{
+public:
     /*
     * Costructor of Dragon class
     *
     * creates a dragon with:
     *          force:25, loot:1000,  damage ability: 100(the maximum amount, the player dies immediately)
+      @return
+ * *      A new Card object
     */
     Dragon();
 
-    /*
-    * Destructor of Dragon class
-    *
-    * 
-    */
-   ~Dragon()=default;
-   
+
+
     /*
     *
     *prints the information of the card
@@ -34,7 +31,7 @@ class Dragon : public BattleCard{
     void printInfo(std::ostream& os ) const override;
 
 
-    void applyEncounter(Player& player) const override; 
+    void applyEncounter(Player& player) const override;
 
     /*
     * create a new Dragon using the copy c’tor
@@ -43,6 +40,19 @@ class Dragon : public BattleCard{
     *
     */
     Card* clone() const override;
+    /*
+   * Destructor of Dragon class
+   *
+   *
+   */
+    ~Dragon()=default;
+
+    /*
+   *
+   *prints the information of the card
+   *
+   */
+    void printInfo(std::ostream& os ) const override;
 
 };
 
